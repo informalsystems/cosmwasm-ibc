4c098eed99cb3e98e6f62b448ee6305211ca9c18
Ranadeep Biswas <mail@rnbguy.at>
2024-09-27 12:03:22 +0200

    use lint tables

--- ibc-clients/cw-context/src/lib.rs
+++ ibc-clients/cw-context/src/lib.rs
@@ -8,15 +8,6 @@
 //! `ibc-go` that supports the `08-wasm` proxy light client.
 
 #![cfg_attr(not(test), deny(clippy::unwrap_used))]
-#![deny(
-    warnings,
-    trivial_casts,
-    trivial_numeric_casts,
-    unused_import_braces,
-    unused_qualifications,
-    rust_2018_idioms
-)]
-#![forbid(unsafe_code)]
 
 pub mod api;
 pub mod context;
