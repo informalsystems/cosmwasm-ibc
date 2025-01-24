45ae138650fba4b22918bdedfe3d8f8791d6dda5
Ranadeep Biswas <mail@rnbguy.at>
2025-01-24 18:01:44 +0100

    MembershipVerifierContainer

--- ibc-clients/cw-context/src/handlers.rs
+++ ibc-clients/cw-context/src/handlers.rs
@@ -1,6 +1,6 @@
 use core::fmt::Display;
 
-use cosmwasm_std::{to_json_binary, Binary};
+use cosmwasm_std::{to_json_binary, Binary, Checksum};
 use ibc_core::client::context::prelude::*;
 use ibc_core::host::types::error::DecodingError;
 use ibc_core::host::types::path::ClientConsensusStatePath;
@@ -16,6 +16,40 @@ use crate::types::{
     VerifyClientMessageResponse, VerifyMembershipMsg, VerifyNonMembershipMsg,
     VerifyUpgradeAndUpdateStateMsg,
 };
+
+#[derive(Debug)]
+pub struct MembershipVerifierContainer {
+    pub state_root: Vec<u8>,
+    pub prefix: Vec<u8>,
+    pub path: Vec<u8>,
+    pub value: Option<Vec<u8>>,
+}
+
+impl MembershipVerifierContainer {
+    pub fn canonical_bytes(self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        let encode_bytes = |mut data: Vec<u8>, output: &mut Vec<u8>| {
+            let len = data.len() as u64;
+            output.extend(len.to_be_bytes());
+            output.append(&mut data);
+        };
+
+        encode_bytes(self.state_root, &mut bytes);
+        encode_bytes(self.prefix, &mut bytes);
+        encode_bytes(self.path, &mut bytes);
+
+        if let Some(v) = self.value {
+            encode_bytes(v, &mut bytes);
+        }
+
+        bytes
+    }
+
+    pub fn checksum(self) -> Checksum {
+        Checksum::generate(&self.canonical_bytes())
+    }
+}
 
 impl<'a, C: ClientType<'a>> Context<'a, C>
 where
